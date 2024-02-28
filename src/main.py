c0cb4fde6ac980d5aa350e3cd5ef8e092e9978f1
Wesllen Sousa Lima <wesllen.wsl@gmail.com>
2024-02-28 15:16:19 -0300

    feat: update score generation function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
 
+import json
+from importlib import resources
+
 from application_model.application_model import generate_application_score
 
-generate_application_score({})
+# load payload data
+with resources.path("application_model.resources", "data.json") as path:
+    print(">>> ", path)
+    with open(path, "rb") as file:
+        payload = file.read()
+
+payload_json = json.loads(payload)
+score = generate_application_score(payload_json)
+print(">>>", score)
