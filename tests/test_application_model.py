79b747e265d3fa7e0d09fe9d7a9e34fc705c3ec7
Wesllen Sousa Lima <wesllen.wsl@gmail.com>
2024-02-28 15:14:10 -0300

    test: improve score generation test function

--- tests/test_application_model.py
+++ tests/test_application_model.py
@@ -1,15 +1,25 @@
 
+import json
 import pytest
+from importlib import resources
+
 from application_model.application_model import generate_application_score
 
 
 @pytest.fixture
 def expected_score():
-    return 0.8973
+    return 0.40108762954840554
 
 
 def test_generate_application_score(expected_score):
     """Test the model output score
     """
-    score = generate_application_score({})
+    # load payload data
+    with resources.path("application_model.resources", "data.json") as path:
+        print(">>> ", path)
+        with open(path, "rb") as file:
+            payload = file.read()
+
+    payload_json = json.loads(payload)
+    score = generate_application_score(payload_json)
     assert score == expected_score, "Score value incorrectly!"
