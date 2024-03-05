02bc2572b73a159de28e50c3e0fa58187020c3d2
Wesllen Sousa Lima <wesllen.wsl@gmail.com>
2024-03-05 11:58:59 -0400

    feat: added new version of application model without city attribute

--- tests/test_application_model.py
+++ tests/test_application_model.py
@@ -18,15 +18,15 @@ def payload():
 @pytest.fixture
 def expected_keys():
     return [
-        'ume-profession', 'ume-zipcode', 'ume-age_on_application', 'ume-segment', 
-        'ume-retailer', 'ume-city', 'ume-state', 'bvsIncome-CLASSRENDAV2', 'bvsIncome-RendaPresumida', 
-        'bvsP5-Score', 'bvsSubP5-Fintechs', 'bvsSubP5-CartaoCredito', 'bvsSubP5-CreditoPessoal', 
-        'bvsSubP5-VAR_MoveisEletrodomesticos', 'bvsSubP5-VAR_VestuarioAcessorios', 'bvsSubP5-FinancialmentoVeiculos'
+        'ume-profession', 'ume-zipcode', 'ume-age_on_application', 'ume-segment', 'ume-retailer', 'ume-state', 
+        'bvsIncome-CLASSRENDAV2', 'bvsIncome-RendaPresumida', 'bvsP5-Score', 'bvsSubP5-Fintechs', 
+        'bvsSubP5-CartaoCredito', 'bvsSubP5-CreditoPessoal', 'bvsSubP5-VAR_MoveisEletrodomesticos', 
+        'bvsSubP5-VAR_VestuarioAcessorios', 'bvsSubP5-FinancialmentoVeiculos'
     ]
 
 @pytest.fixture
 def expected_score():
-    return 0.40108762954840554
+    return 0.41316487120280165
 
 
 def test_payload_structure(payload, expected_keys):
@@ -34,8 +34,8 @@ def test_payload_structure(payload, expected_keys):
     """
     keys = list(payload.keys())
 
-    assert len(keys) == len(expected_keys), "Number of columns of the payload are incorrect!"
-    assert keys == expected_keys, "Columns name of the payload are incorrect!"
+    assert len(keys) == len(expected_keys), "Number of payload columns are incorrect!"
+    assert keys == expected_keys, "Payload columns names are incorrect!"
 
 def test_generate_application_score(payload, expected_score):
     """Test the model output score
